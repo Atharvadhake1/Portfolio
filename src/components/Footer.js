bc2bcca6e7a808043cdb0024a6104ae264132f1b
Atharva <atharvadhake01@gmail.com>
2024-11-15 23:45:50 +0530

    footer done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,80 +1,40 @@
 import { Container, Row, Col } from "react-bootstrap";
-import logo from "../assets/img/logo.svg";
 import navIcon1 from "../assets/img/nav-icon1.svg";
-import navIcon2 from "../assets/img/nav-icon2.svg";
+import navIcon2 from "../assets/img/github.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
+import navIcon4 from "../assets/img/leetcode.svg";
 
 export const Footer = () => {
   return (
     <footer className="footer">
-      <div className="text-white-500 flex gap-2">
-        <p>Terms & Conditions | </p>
-        <p>|</p>
-        <p>Privacy Policy</p>
-      </div>
+      <Container>
+        <Row className="align-items-center">
+          {/* Left Column - Text */}
+          <Col sm={4} className="text-start">
+            <p>
+              Loosely designed in <strong>Figma</strong> and coded in <strong>Visual Studio Code</strong> by yours truly.
+              Built with <strong>ReactJS</strong>, deployed with <strong>Vercel</strong>.
+              All text is set in the <strong>Inter</strong> typeface.
+            </p>
+          </Col>
 
-      <div className="flex gap-3">
-        <div className="social-icon">
-          <a
-            href="https://github.com/Atharvadhake1"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="w-full h-full flex items-center justify-center"
-          >
-            <img
-              src="../assets/img/github.svg"
-              alt="github"
-              className="w-1/2 h-1/2"
-            />
-          </a>
-        </div>
-        <div className="social-icon">
-          <a
-            href="https://www.linkedin.com/in/atharva-dhake-155160258/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="w-full h-full flex items-center justify-center"
-          >
-            <img
-              src="/assets/linkedin.svg"
-              alt="linkedin"
-              className="w-1/2 h-1/2"
-            />
-          </a>
-        </div>
-        <div className="social-icon">
-          <a
-            href="https://leetcode.com/u/atharva_dhake_/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="w-full h-full flex items-center justify-center"
-          >
-            <img
-              src="../assets/leetcode.svg"
-              alt="leetcode"
-              className="w-1/2 h-1/2"
-            />
-          </a>
-        </div>
-        <div className="social-icon">
-          <a
-            href="https://www.instagram.com/atharva_dhake_/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="w-full h-full flex items-center justify-center"
-          >
-            <img
-              src="/assets/img/instagram.svg"
-              alt="instagram"
-              className="w-1/2 h-1/2"
-            />
-          </a>
-        </div>
-      </div>
+          {/* Center Column - Social Links */}
+          <Col sm={4} className="text-center">
+            <div className="social-icon">
+              <a href="https://www.linkedin.com/in/atharva-dhake-155160258/" target="_blank"><img src={navIcon1} alt="Icon" /></a>
+              <a href="https://github.com/Atharvadhake1"><img src={navIcon2} alt="Icon" /></a>
+              <a href="https://leetcode.com/u/atharva_dhake_/"><img src={navIcon4} alt="Icon" /></a>
+              <a href="https://www.instagram.com/atharva_dhake_/"><img src={navIcon3} alt="Icon" /></a>
 
-      <p className="text-white-500">
-        © 2024 Atharva Dhake. All rights reserved.
-      </p>
+            </div>
+          </Col>
+
+          {/* Right Column - Copyright */}
+          <Col sm={4} className="text-end">
+            <p>© 2024 Atharva Dhake. All Rights Reserved.</p>
+          </Col>
+        </Row>
+      </Container>
     </footer>
   );
 };
